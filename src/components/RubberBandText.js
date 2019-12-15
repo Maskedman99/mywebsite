2299b5efbb137620087c2bda2375e82a8b483dba
Rohit <rohitmpaul@gmail.com>
2019-12-15 19:21:17 +0530

    Initial Commit

---
+++ src/components/RubberBandText.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled, { keyframes } from 'styled-components';
+import { rubberBand, rotateInDownLeft } from 'react-animations';
+import './RubberBandText.css'
+
+const RubberBand = styled.div`animation: 2s ${keyframes`${rubberBand}`}`;
+const Rotate = styled.div`animation: 0.5s ${keyframes`${rotateInDownLeft}`}`;
+
+class RubberBandText extends React.Component {
+    render(){
+        const arr = this.props.text.split("");
+
+        return(
+        <div>
+        {
+            arr.map((item, key)=>(
+                <Rotate><RubberBand className="RubberBand"><p>{item}</p></RubberBand></Rotate>
+            ))
+        }
+        </div>
+        );
+    }
+}
+
+export default RubberBandText;
