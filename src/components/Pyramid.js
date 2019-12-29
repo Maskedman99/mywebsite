cfa6a66bc2e86273af9859e96cf9cff67605fb23
Rohit <rohitmpaul@gmail.com>
2019-12-30 01:31:36 +0530

    add pyramid animation

---
+++ src/components/Pyramid.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './Pyramid.css'
+
+class Pyramid extends React.Component{
+    render() {
+        return(
+            <div class="pyramidWrap">
+                <section id="pyramid">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </section>
+            </div>
+        );
+    }
+}
+export default Pyramid;
