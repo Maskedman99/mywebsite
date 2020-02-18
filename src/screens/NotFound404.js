e9ab9beae6264e2429fccb02d20519792babda15
Rohit <rohitmpaul@gmail.com>
2020-02-18 21:00:47 +0530

    add CSS

--- src/screens/NotFound404.js
+++ src/screens/NotFound404.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+
+import "../css/NotFound404.css"
 
 const NotFound404 = () => {
     return(
-        <div>404: Page not found</div>
+        <div className='container-404'>
+        <div className='h-404'>404</div>
+        <div className='p-404'>Oops! Something went wrong.</div>
+        <div className="linkdiv-404">
+            <Link to="/" className="link-404">Go back to Initial page.</Link>
+        </div>
+        </div>
     )
 }
 
