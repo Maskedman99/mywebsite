325a6cc89a48d1873f0ea86d0a28bed38aa7bdfc
Rohit <rohitmpaul@gmail.com>
2020-05-08 12:16:38 +0530

    add displaying markdown ability

--- src/screens/Articles.js
+++ src/screens/Articles.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import marked from 'marked';
+import DOMPurify from 'dompurify'
 
 const Article = () => {
         return(
-            <div>Aricle</div>
+            <div>
+            <div dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(marked('# Marked in the browser\n\nRendered by **marked**.'))}} />
+            </div>
         );
     }
 
