07bbe2560ce4b199657a802f5c16bac01fa3eedd
Rohit <rohitmpaul@gmail.com>
2020-08-07 01:01:08 +0530

    add colors and optimize the about icons

--- src/components/AboutUrls.js
+++ src/components/AboutUrls.js
@@ -1,50 +1,61 @@
 import React from 'react';
 
-import github from '../assets/github.svg';
-import linkedin from '../assets/linkedin.svg';
-import envelope from '../assets/envelope.svg';
-import codepen from '../assets/codepen.svg';
-import sourcerer from '../assets/sourcerer.svg';
+import github from '../assets/github.json';
+import linkedin from '../assets/linkedin.json';
+import mail from '../assets/mail.json';
+import codepen from '../assets/codepen.json';
+import sourcerer from '../assets/sourcerer.json';
 
 import '../css/AboutUrls.css';
 
 const AboutUrls = ({orientation}) => {
   return (
-    <div className={orientation === 'vertical' ? 'About-footer-vertical' : 'About-footer-horizontal'}>
+    <div
+      className={orientation === 'vertical' ? 'About-footer-vertical' : 'About-footer-horizontal'}>
       <a
         className="About-links"
         target="_blank"
         rel="noopener noreferrer"
         href="https://github.com/Maskedman99">
-        <img className="About-image" src={github} alt="" />
+        <svg className="About-image" viewBox={github.viewBox}>
+          <path d={github.path} />
+        </svg>
       </a>
       <a
         className="About-links"
         target="_blank"
         rel="noopener noreferrer"
         href="https://www.linkedin.com/in/rohit-prasad-819a8015a/">
-        <img className="About-image" src={linkedin} alt="" />
+        <svg className="About-image" viewBox={linkedin.viewBox}>
+          <path d={linkedin.path} />
+        </svg>
       </a>
       <a
         className="About-links"
         target="_blank"
         rel="noopener noreferrer"
         href="https://codepen.io/Maskedman99/pens/">
-        <img className="About-image" src={codepen} alt="" />
+        <svg className="About-image" viewBox={codepen.viewBox}>
+          <path d={codepen.path} />
+        </svg>
       </a>
       <a
         className="About-links"
         target="_blank"
         rel="noopener noreferrer"
         href="mailto:rohitmpaul@gmail.com">
-        <img className="About-image" src={envelope} alt="" />
+        <svg className="About-image" viewBox={mail.viewBox}>
+          <path d={mail.path} />
+        </svg>
       </a>
       <a
         className="About-links"
         target="_blank"
         rel="noopener noreferrer"
         href="https://sourcerer.io/maskedman99">
-        <img className="About-image" src={sourcerer} alt="" />
+        <svg className="About-image" viewBox={sourcerer.viewBox}>
+          <path d={sourcerer.path} />
+        </svg>
       </a>
     </div>
   );
