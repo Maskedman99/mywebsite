3f4e938277e5929965df63ce1c48638d7fadb69c
Rohit <rohitmpaul@gmail.com>
2020-08-06 21:50:22 +0530

    add styles to main texts

--- src/screens/Main.js
+++ src/screens/Main.js
@@ -8,11 +8,17 @@ const Main = () => {
   return (
     <div className="App-main">
       <AboutUrls orientation={'vertical'} />
-      <>
-        <div className="App-main-text">Hi, I'm </div>
-        <div className="App-main-text">Rohit Prasad </div>
-        <div className="App-main-text">I create websites and mobile apps for fun</div>
-      </>
+      <div className="App-main-text-container">
+        <div className="App-main-normaltext">Hi, I'm</div>
+        <div className="App-main-title" style={{color: 'aqua'}}>
+          Rohit Prasad.
+        </div>
+        <div className="App-main-title" style={{color: 'aquamarine'}}>
+          I build things for the web and mobile.
+        </div>
+        <div>Currently pursuing my degree in Computer Science and Engineering</div>
+        <div>at Model Engineering College, Thrikkakara.</div>
+      </div>
     </div>
   );
 };
